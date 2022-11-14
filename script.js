ae485b4a33bbd653a77eca7803f7efeb0c5ff7ae
Brendow Lincoln <linconlbrendow@gmail.com>
2022-11-14 11:51:43 -0300

    Finalizing extra class 2 assignment

--- script.js
+++ script.js
@@ -4,33 +4,72 @@ var selectedOperation = null;
 var inputNumber = document.getElementById("input-number");
 
 function setValue(value) {
-
     inputNumber.value = inputNumber.value + String(value);
     console.log(value);
 }
 
 function setOperation(operation) {
-    clearInput();
-
-    selectedOperation = operation;
-
-    switch (operation) {
-        case "/":
-            
-            break;
+    if(!firstValue && !selectedOperation) {
+        firstValue = Number(inputNumber.value);
+        selectedOperation = operation;
     }
-    selectedOperation = operation;
 
+    clearInput();
 }
+
+function clearValues() {
+    firstValue = null;
+    secondValue = null;
+    selectedOperation = null;
+    clearInput();
+}
+
+
 
 function clearInput() {
     inputNumber.value = "";
 }
 
 function calculate() {
+    if(!firstValue) {
+        showMessage("Valores não definidos");
+        return;
+    }
+    secondValue = Number(inputNumber.value);
+
+    let result = null;
+    
     switch (selectedOperation) {
         case "/":
-            
+            if(secondValue === 0) {
+                showMessage("Impossível fazer uma divisão por 0.");
+                return;
+            }
+
+            result = firstValue / secondValue;
+            inputNumber.value = String(result);
+            break;
+
+        case "*":
+            result = firstValue * secondValue;
+            inputNumber.value = String(result);
+            break;
+
+        case "-":
+            result = firstValue - secondValue;
+            inputNumber.value = String(result);
+            break;
+
+        case "+":
+            result = firstValue + secondValue;
+            inputNumber.value = String(result);
             break;
     }
+
+    showMessage("");
+}
+
+var showMessage = (message) => {
+    let messageDiv = document.getElementById("info-message");
+    messageDiv.innerText = message;
 }
